99b35993b7de0347f0e1f6744ca2127f559b0394
heimedina <heidimedina23@gmail.com>
2025-04-22 12:33:28 +0200

    modified Pais

--- src/main/java/com/ideas/persona/controller/rest/PaisController.java
+++ src/main/java/com/ideas/persona/controller/rest/PaisController.java
@@ -1,10 +1,12 @@
 package com.ideas.persona.controller.rest;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +23,11 @@ public class PaisController {
 	@GetMapping
 	private ResponseEntity<List<Pais>> getallPaises(){
 		return ResponseEntity.ok(paisService.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	private ResponseEntity<Optional<Pais>> getPaisesById(@PathVariable("id") int id){
+		return ResponseEntity.ok(paisService.findById(id));
 	}
 
 }
