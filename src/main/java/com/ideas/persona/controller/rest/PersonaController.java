1e4c3aa6fc4d36d2ab61c76056e28658b7ad1ea7
heimedina <heidimedina23@gmail.com>
2025-04-22 12:59:31 +0200

    added Persona Service/Controller

--- src/main/java/com/ideas/persona/controller/rest/PersonaController.java
+++ src/main/java/com/ideas/persona/controller/rest/PersonaController.java
@@ -1,5 +1,40 @@
 package com.ideas.persona.controller.rest;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ideas.persona.models.Persona;
+import com.ideas.persona.service.PersonaService;
+
+@RestController
+@RequestMapping("/personas")
 public class PersonaController {
 
+	@Autowired
+	private PersonaService personaService;
+	
+	@GetMapping
+	private ResponseEntity<List<Persona>> getAllPersonas(){
+		return ResponseEntity.ok(personaService.findAllPersonas());
+	}
+	
+	@PostMapping
+	private ResponseEntity<Persona> savePersona (@RequestBody Persona persona){
+		try {
+			Persona personaPersiste = personaService.guardarPersona(persona);
+			return ResponseEntity.created(new URI("/personas"+personaPersiste.getId())).body(personaPersiste);
+		} catch (URISyntaxException e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+		}
+	}
 }
